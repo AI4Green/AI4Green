6534ab8e768d88f237500a8c87fbf91d0bc586a3
Joseph Davies <Joseph.davies@nottingham.ac.uk>
2023-11-01 12:21:47 +0000

    combined related queries to domains, simplified function names

--- Webapp/sources/services/queries/__init__.py
+++ Webapp/sources/services/queries/__init__.py
@@ -1,9 +1,1 @@
-from . import (
-    compound,
-    compound_data_error_report,
-    reaction,
-    user,
-    workbook,
-    workgroup,
-    workgroup_request,
-)
+from . import compound, reaction, user, workbook, workgroup
