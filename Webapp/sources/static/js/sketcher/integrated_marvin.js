a556527deaa52a37c084d2baf7badbed1d8f2c94
Joseph Davies <Joseph.davies@nottingham.ac.uk>
2023-12-19 11:48:31 +0000

    timer added to marvin js call

--- Webapp/sources/static/js/sketcher/integrated_marvin.js
+++ Webapp/sources/static/js/sketcher/integrated_marvin.js
@@ -1,14 +1,14 @@
-let marvin
+let marvin;
 /**
  * Assigns the global variable 'marvin' to the marvin sketcher object and prepares the sketcher ready for use
  */
-async function setupNewMarvinSketcher(){
-    let marvinKey = $("#marvin-js-key").val()
-    marvin = await newMarvinSketcher("#marvin-sketcher", marvinKey)
-    if (marvin) {
-        marvin.setDisplaySettings({"toolbars": "education"});
-        $("#marvin-sketcher").hide()
-    }
+async function setupNewMarvinSketcher() {
+  let marvinKey = $("#marvin-js-key").val();
+  marvin = await newMarvinSketcher("#marvin-sketcher", marvinKey);
+  if (marvin) {
+    marvin.setDisplaySettings({ toolbars: "education" });
+    $("#marvin-sketcher").hide();
+  }
 }
 
 /**
@@ -18,16 +18,34 @@ async function setupNewMarvinSketcher(){
  * @return {Object} MarvinJS Chemical Editor object with access to the MarvinJS methods
  */
 async function newMarvinSketcher(IDSelector, marvinKey) {
-    let marvinKey1 = await getMarvinKey()
-    try {
-        await $.getScript("https://marvinjs.chemicalize.com/v1/client.js")
-        await $.getScript(`https://marvinjs.chemicalize.com/v1/${marvinKey1}/client-settings.js`)
-    } catch(error) {
-        console.log(error.status)
-        abortMarvinSketcherCreation()
-        return null
+  let marvinKey1 = await getMarvinKey();
+
+  try {
+    // Use Promise.race to set a timeout for the entire operation
+    const timeoutPromise = new Promise((_, reject) => {
+      setTimeout(() => reject(new Error("Timeout")), 5000);
+    });
+
+    // Use Promise.all to wait for both scripts to load or for the timeout
+    await Promise.all([
+      $.getScript("https://marvinjs.chemicalize.com/v1/client.js"),
+      $.getScript(
+        `https://marvinjs.chemicalize.com/v1/${marvinKey1}/client-settings.js`,
+      ),
+      timeoutPromise, // Timeout promise
+    ]);
+  } catch (error) {
+    // Check if the error is due to a timeout
+    if (error.message === "Timeout") {
+      console.log("Operation timed out");
+    } else {
+      console.log(error.status);
+      abortMarvinSketcherCreation();
     }
-    return ChemicalizeMarvinJs.createEditor(IDSelector)
+    return null;
+  }
+
+  return ChemicalizeMarvinJs.createEditor(IDSelector);
 }
 
 /**
@@ -35,14 +53,14 @@ async function newMarvinSketcher(IDSelector, marvinKey) {
  * @return {Promise<string>} - The marvin JS license key for accessing their API services
  */
 async function getMarvinKey() {
-    let marvinJsKey
-    await $.ajax({
-        method: "POST",
-        url: "/get_marvinjs_key",
-    }).then(function (response) {
-        marvinJsKey = response.marvinjs_key
-    });
-    return marvinJsKey
+  let marvinJsKey;
+  await $.ajax({
+    method: "POST",
+    url: "/get_marvinjs_key",
+  }).then(function (response) {
+    marvinJsKey = response.marvinjs_key;
+  });
+  return marvinJsKey;
 }
 
 /**
@@ -50,47 +68,46 @@ async function getMarvinKey() {
  * @return {Promise<void>}
  */
 async function switchToMarvinSketcher() {
-    $("#ketcher-sketcher").hide()
-    if (marvin) {
-        await exportSmilesFromKetcherToMarvin()
-        $("#marvin-sketcher").show()
-    } else {
-        alert("Marvin JS is temporarily unavailable")
-    }
+  $("#ketcher-sketcher").hide();
+  if (marvin) {
+    await exportSmilesFromKetcherToMarvin();
+    $("#marvin-sketcher").show();
+  } else {
+    alert("Marvin JS is temporarily unavailable");
+  }
 }
 
 /**
  * Switches back to Ketcher, disables further switching and hides the loading circle again
  */
-function abortMarvinSketcherCreation(){
-    // runs when
-    alert("Marvin JS is temporarily unavailable")
-    $("#ketcher-select").prop("checked", true)
-    $("#marvin-select").prop("disabled", true)
+function abortMarvinSketcherCreation() {
+  // alert("Marvin JS is temporarily unavailable")
+  $("#ketcher-select").prop("checked", true);
+  // $("#marvin-select").prop("disabled", true)
 }
 
 /**
  * Autosave when the sketcher is edited
  */
-function setupMarvinAutosave(){
-    marvin.on('molchange', function () {
-        // sketcher arg to true to save just smiles
-        setTimeout(autoSaveCheck(null, true), 100)
-    });
+function setupMarvinAutosave() {
+  marvin.on("molchange", function () {
+    // sketcher arg to true to save just smiles
+    setTimeout(autoSaveCheck(null, true), 100);
+  });
 }
 
 /**
  * Exports structures from ketcher to marvin via SMILES
  * @return {Promise<void>}
  */
-async function exportSmilesFromKetcherToMarvin(){
-    let smiles
-    if (getKetcher()) {
-        smiles = await exportSmilesFromKetcher()
-        if (smiles){
-            marvin.importStructure("cxsmiles", smiles);
-        }
+async function exportSmilesFromKetcherToMarvin() {
+  let smiles;
+  if (getKetcher()) {
+    smiles = await exportSmilesFromKetcher();
+    if (smiles) {
+      marvin.importStructure("cxsmiles", smiles);
     }
+  }
 }
 
 /**
@@ -99,26 +116,28 @@ async function exportSmilesFromKetcherToMarvin(){
  * in format: "reactant1.reactantX>reagent1.reagentX>product1.productX"
  */
 function exportSmilesFromMarvin() {
-    return marvin.exportStructure('cxsmiles', {'extra': 'f'}).then(function (smiles) {
-        return smiles
+  return marvin
+    .exportStructure("cxsmiles", { extra: "f" })
+    .then(function (smiles) {
+      return smiles;
     });
 }
 
 /**
  * Enters the example reaction into the Marvin editor
  */
-function marvinExampleSmiles(){
-    let smiles = "OC(=O)C1=CC=CC=C1.CCN>>CCNC(=O)C1=CC=CC=C1";
-    marvin.importStructure("cxsmiles", smiles);
+function marvinExampleSmiles() {
+  let smiles = "OC(=O)C1=CC=CC=C1.CCN>>CCNC(=O)C1=CC=CC=C1";
+  marvin.importStructure("cxsmiles", smiles);
 }
 
 /**
  * Makes an image of the current reaction scheme and saves to a hidden HTML input
  */
 function exportMarvinImage() {
-    let settings = {'width': 600, 'height': 400};
-    marvin.exportStructure("jpeg", settings).then(function (source) {
-        sessionStorage.setItem("reactionSchemeImage", source)
-        //$("#js-reaction-scheme-image").val(source);
-    });
+  let settings = { width: 600, height: 400 };
+  marvin.exportStructure("jpeg", settings).then(function (source) {
+    sessionStorage.setItem("reactionSchemeImage", source);
+    //$("#js-reaction-scheme-image").val(source);
+  });
 }
