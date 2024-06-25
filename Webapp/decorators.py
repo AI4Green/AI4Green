d557c08f088bbe68a466d0d23cc24c1b000bf73c
pczjh2 <joe.heeley@nottingham.ac.uk>
2024-06-25 16:42:05 +0100

    PI required decorator

--- Webapp/decorators.py
+++ Webapp/decorators.py
@@ -4,17 +4,19 @@ from flask_login import current_user, login_required
 from sources import services
 
 
-def _get_workgroup():
-    return request.args.get("workgroup")
+def _get_workgroup(workgroup):
+    if "workgroup" in request.args:
+        return request.args.get("workgroup")
+    else:
+        return workgroup
 
 
 def principal_investigator_required(f):
     @wraps(f)
-    def decorated_function(*args, **kwargs):
-        workgroup = _get_workgroup()
-        print(workgroup)
+    def decorated_function(workgroup, *args, **kwargs):
+        workgroup = _get_workgroup(workgroup)
         if services.workgroup.get_user_type(workgroup) != "principal_investigator":
             flash("You do not have permission to view this page")
             return redirect(url_for("main.index"))
-        return f(*args, **kwargs)
+        return f(workgroup, *args, **kwargs)
     return decorated_function
