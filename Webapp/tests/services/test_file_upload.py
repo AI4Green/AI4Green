60a865e2213d0a90e03a5661ba9a8d67967cc04b
pczjh2 <joe.heeley@nottingham.ac.uk>
2024-11-07 12:02:10 +0000

    remove magic

--- Webapp/tests/services/test_file_upload.py
+++ Webapp/tests/services/test_file_upload.py
@@ -5,7 +5,6 @@ import pytest
 import pytest_mock
 from flask.testing import FlaskClient
 from tests.utils import login
-from magic import Magic
 
 IN_GITHUB_ACTIONS = os.getenv("GITHUB_ACTIONS") == "true"
 
