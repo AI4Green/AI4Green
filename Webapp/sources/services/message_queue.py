533c87680031d49bc9289e22cf8bfdd2969e879b
Daniel Lea <Daniel.Lea@nottingham.ac.uk>
2025-06-05 10:41:34 +0100

    logging producer

--- Webapp/sources/services/message_queue.py
+++ Webapp/sources/services/message_queue.py
@@ -1,5 +1,6 @@
 import json
 from typing import Any, Optional
+from flask import current_app
 from kafka import KafkaProducer
 
 
@@ -37,8 +38,27 @@ class QueueProducer(BaseQueueProducer):
         """Send a message to the message queue with the given topic.
 
         Args:
-            topic (Optional[str]): The topic to send the message to.
-            msg (Optional[Any]): The message to send to the queue.
+            topic (str, optional): The topic to send the message to.
+            msg (Any, optional): The message to send to the queue.
         """
         self.producer.send(topic, value=msg)
         self.producer.flush()
+
+
+class LoggingQueueProducer(BaseQueueProducer):
+    """
+    A queue producer designed to be used in testing situations where
+    actually sending messages to a message queue is not required.
+
+    This producer simply logs the messages with the Flask logger instance.
+    """
+
+    def send(self, topic: Optional[str] = None, msg: Optional[Any] = None):
+        """Send a message to the standard output using the built-in logger
+        in Flask.
+
+        Args:
+            topic (str, optional): This is ignored in this class. Defaults to None.
+            msg (Any, optional): The message to send to the queue. Defaults to None.
+        """
+        current_app.logger.info(msg=msg)
