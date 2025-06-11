904b7d8e5c742c6966925fe89872f36fc54d4b56
Daniel Lea <Daniel.Lea@nottingham.ac.uk>
2025-06-11 15:03:01 +0100

    deserialise method on interface

--- Webapp/sources/services/message_queue.py
+++ Webapp/sources/services/message_queue.py
@@ -78,3 +78,20 @@ class MessageSerdeMixin:
             dict: The message class formated with shcema and payload.
         """
         raise NotImplementedError
+
+    @staticmethod
+    def deserialise(data: dict) -> Any:
+        """Convert a message from a JSON object to a message object.
+
+        The message object is a class decorated with `@dataclass`.
+
+        Args:
+            data (dict): The json to decode.
+
+        Raises:
+            NotImplementedError: You must implement this method in classes that implement this mixin.
+
+        Returns:
+            Any: The message class from the JSON.
+        """
+        raise NotImplementedError
