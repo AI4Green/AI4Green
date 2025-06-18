a35325b980c42d3db536d41ed1ea700f3495d7b3
Daniel Lea <Daniel.Lea@nottingham.ac.uk>
2025-06-18 14:00:12 +0100

    test download fails if workgroup param is not an integer

--- Webapp/tests/services/test_audit_logs.py
+++ Webapp/tests/services/test_audit_logs.py
@@ -14,3 +14,18 @@ def test_download_fails_without_topic(client: FlaskClient):
     assert isinstance(response.json, dict)
     assert "error" in response.json
     assert response.json["error"] == expected_msg["error"]
+
+
+def test_download_fails_with_workgroup_as_non_integer(client: FlaskClient):
+    login(client)
+    expected_code = 400
+    expected_msg = {"error": "workgroup must an integer"}
+    response = client.get(
+        "/audit_log/download",
+        query_string={"topic": "test_topic", "workgroup": "hello"},
+    )
+
+    assert response.status_code == expected_code
+    assert isinstance(response.json, dict)
+    assert "error" in response.json
+    assert response.json["error"] == expected_msg["error"]
