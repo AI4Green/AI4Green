8790561936d93c1548d3252943a9395a4e0aac2e
Daniel Lea <Daniel.Lea@nottingham.ac.uk>
2025-07-23 16:57:21 +0100

    default to target main db

--- Webapp/migrations/utils.py
+++ Webapp/migrations/utils.py
@@ -1,11 +1,11 @@
-def perform_migration(db_name: str, target: str = "default") -> bool:
+def perform_migration(db_name: str, target: str = "db") -> bool:
     """Create a boolean to determin if a migration should be performed on the
     named database (`db_name`). If `db_name` and `target` match, the migration
     can be performed.
 
     Args:
         db_name (str): The name of the database to migrate.
-        target (str, optional): The intended database to migrate. Defaults to "default".
+        target (str, optional): The intended database to migrate. Defaults to "db".
 
     Returns:
         bool: Whether the migration can be performed.
