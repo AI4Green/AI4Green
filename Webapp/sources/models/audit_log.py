3249224c5cf65fff81737b29c12f299dd57a0d51
Daniel Lea <Daniel.Lea@nottingham.ac.uk>
2025-07-24 13:47:59 +0100

    audit log table and migrations

--- Webapp/sources/models/audit_log.py
+++ Webapp/sources/models/audit_log.py
@@ -5,6 +5,9 @@ from .base import Model
 
 class AuditLogEvent(Model):
     __tablename__ = "AuditLogEvent"
-    __bind_name__ = "audit_log"
+    __bind_key__ = "audit_log"
 
     id = db.Column(db.Integer, primary_key=True)
+    event_time = db.Column(db.DateTime, nullable=False)
+    event_type = db.Column(db.Text, nullable=False)
+    message = db.Column(db.JSON, nullable=False)
