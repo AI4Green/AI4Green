f9b5343452b9af4cee6a54b0f3317f7206cb8434
Daniel Lea <Daniel.Lea@nottingham.ac.uk>
2025-07-28 13:55:37 +0100

    base producer in with producers

--- Webapp/sources/services/message_queue/producers.py
+++ Webapp/sources/services/message_queue/producers.py
@@ -1,6 +1,22 @@
 from typing import Any, Optional
 from kafka import KafkaProducer
-from sources.services.message_queue.base import BaseQueueProducer
+
+
+class BaseQueueProducer:
+    """
+    The interface for message queue producer classes. It provides a
+    `send` method in which the inheriting class defines logic to send
+    messages to some kind of message queue service (e.g. RabbitMQ, Redis, etc.).
+    """
+
+    def send(self, topic: Optional[str] = None, msg: Optional[Any] = None):
+        """Send a message to the message queue with the given topic.
+
+        Args:
+            topic (Optional[str]): The topic to send the message to.
+            msg (Optional[Any]): The message to send to the queue.
+        """
+        raise NotImplementedError
 
 
 class QueueProducer(BaseQueueProducer):
