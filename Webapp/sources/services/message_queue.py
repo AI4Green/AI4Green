ae4bde6e833ddaa756ca731de06f9d6d752267d9
Daniel Lea <Daniel.Lea@nottingham.ac.uk>
2025-07-28 13:34:21 +0100

    move the message mxin to a new module

--- Webapp/sources/services/message_queue.py
+++ Webapp/sources/services/message_queue.py
@@ -1,44 +1,10 @@
 import json
 import time
 from collections import defaultdict
-from typing import Any
 
 from kafka import KafkaConsumer
 from sources.services.message_queue.base import BaseQueueProducer
 from sources import services
-
-
-class MessageSerdeMixin:
-    def serialise(self) -> dict:
-        """Convert a message into a JSON object with a schema and payload.
-
-        The schema is an object that lists the fields and their types.
-        The payload is an object representing the message class in JSON format.
-
-        Raises:
-            NotImplementedError: You must implement this method in classes that implement this mixin.
-
-        Returns:
-            dict: The message class formated with shcema and payload.
-        """
-        raise NotImplementedError
-
-    @staticmethod
-    def deserialise(data: dict) -> Any:
-        """Convert a message from a JSON object to a message object.
-
-        The message object is a class decorated with `@dataclass`.
-
-        Args:
-            data (dict): The JSON to decode.
-
-        Raises:
-            NotImplementedError: You must implement this method in classes that implement this mixin.
-
-        Returns:
-            Any: The message class from the JSON.
-        """
-        raise NotImplementedError
 
 
 class QueueConsumer:
