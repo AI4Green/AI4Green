ed57c09bbbaeb2745c109249036dc1d860424716
Daniel Lea <Daniel.Lea@nottingham.ac.uk>
2025-08-07 15:03:37 +0100

    azure queue consumer class

--- Webapp/sources/services/message_queue/consumers.py
+++ Webapp/sources/services/message_queue/consumers.py
@@ -1,9 +1,11 @@
 import json
 import time
+from typing import List
+from azure.storage.queue import QueueServiceClient, QueueMessage
 from kafka import KafkaConsumer
 
 
-class BaseConsumer:
+class BaseQueueConsumer:
 
     def poll(self, poll_duration_sec: int = 60):
         """Poll messages from topics and returns them.
@@ -14,7 +16,7 @@ class BaseConsumer:
         raise NotImplementedError
 
 
-class QueueConsumer(BaseConsumer):
+class QueueConsumer(BaseQueueConsumer):
     """This class is a service which is used to receive messages from a kafka cluster."""
 
     def __init__(self, hostname, topic):
@@ -53,3 +55,57 @@ class QueueConsumer(BaseConsumer):
         print(f"Collected {len(messages)} messages.")
 
         return messages
+
+
+class AzureQueueConsumer(BaseQueueConsumer):
+    """
+    A queue consumer for reading messages from Azure Queue Storage.
+    """
+
+    def __init__(self, connection_str: str, queue_name: str, max_messages: int = 100):
+        """Create a producer for Azure Queue Storage
+
+        Args:
+            connection_str (str): The connection string for the queue storage resource.
+            queue_name (str): The name of the queue to read from.
+        """
+        self.producer = QueueServiceClient.from_connection_string(connection_str)
+        self.queue_name = queue_name
+        self.max_messages = max_messages
+
+    def poll(self, poll_duration_sec=60):
+        """Poll messages from topics and returns them.
+
+        Args:
+            poll_duration_sec (int): Length of time to poll kafka for, in seconds
+        """
+        messages = []
+
+        client = self.producer.get_queue_client(self.queue_name)
+
+        end_time = time.time() + poll_duration_sec
+        while time.time() < end_time:
+            # Read the messages from the queue
+            msg_pack = [
+                message
+                for message in client.receive_messages(max_messages=self.max_messages)
+            ]
+            for msg in msg_pack:
+                messages.append(msg.content)
+            # Clear the old mesages
+            self._clear_messages(client, msg_pack)
+
+        client.close()
+
+        return messages
+
+    def _clear_messages(self, sc: QueueServiceClient, messages: List[QueueMessage]):
+        """Delete messages from the Azure Queue Storage.
+
+        Args:
+            sc (QueueServiceClient): The service client for Azure Queue Storage.
+            messages (List[QueueMessage]): The list of messages to delete.
+        """
+        client = sc.get_queue_client(self.queue_name)
+        for message in messages:
+            client.delete_message(message)
