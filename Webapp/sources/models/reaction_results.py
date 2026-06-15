a62f3b1e0cc812795fbee332cdd45ecd0b7b3769
pczjh2 <joe.heeley@nottingham.ac.uk>
2026-06-15 09:43:24 +0100

    result db model

---
+++ Webapp/sources/models/reaction_results.py
@@ -0,0 +1,37 @@
+from sources.extensions import db
+
+from .base import Model
+
+
+class ReactionResultType(Model):
+    __tablename__ = "ReactionResultType"
+
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String, nullable=False)
+    description = db.Column(db.String, nullable=False)
+    value_type = db.Column(db.String, nullable=False)
+    default_unit = db.Column(db.String, nullable=False)
+
+    workgroup_id = db.Column(
+        db.Integer, db.ForeignKey("WorkGroup.id"), nullable=True, index=True
+    )
+    workgroup = db.relationship(
+        "WorkGroup",
+        backref="reaction_result_types",
+    )
+
+    workbook_id = db.Column(
+        db.Integer, db.ForeignKey("WorkBook.id"), nullable=True, index=True
+    )
+    workbook = db.relationship(
+        "WorkBook",
+        backref="reaction_result_types",
+    )
+
+    creator_person_id = db.Column(
+        db.Integer, db.ForeignKey("Person.id"), nullable=True, index=True
+    )
+    creator_person = db.relationship(
+        "Person",
+        backref="reaction_result_types",
+    )
