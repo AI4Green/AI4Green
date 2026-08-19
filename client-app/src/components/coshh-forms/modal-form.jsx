22fd3236ae79fe5d0aaad3ae3ca690243540146e
pczjh2 <joe.heeley@nottingham.ac.uk>
2026-08-19 09:37:53 +0100

    remove section type

--- client-app/src/components/coshh-forms/modal-form.jsx
+++ client-app/src/components/coshh-forms/modal-form.jsx
@@ -121,7 +121,7 @@ export const CreateOrEditProjectTypeModal = () => {
       innerRef={formRef}
       initialValues={initialValues()}
       onSubmit={handleSubmit}
-      validationSchema={validationSchema(projectTypes, projectType)}
+      validationSchema={validationSchema(coshhForms, projectType)}
     >
       <Form noValidate>
         <VStack align="stretch" spacing={4}>
@@ -150,7 +150,7 @@ export const CreateOrEditProjectTypeModal = () => {
               <MultiSelectField
                 name="source"
                 label="Import from"
-                options={projectTypes
+                options={coshhForms
                   .filter((x) => x.stage === STAGES.Ready)
                   .map((projectType) => ({
                     label: projectType.name,
